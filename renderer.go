16f594809bc5f24543b14d3b932362dc651258d4
Masaaki Goshima <goccy54@gmail.com>
2020-10-19 17:34:05 +0900

    render html

--- renderer.go
+++ renderer.go
@@ -3,7 +3,10 @@ package servicetracer
 import (
 	"bytes"
 	"fmt"
+	"io/ioutil"
 	"log"
+
+	"text/template"
 
 	"github.com/goccy/go-graphviz"
 	"github.com/goccy/go-graphviz/cgraph"
@@ -18,11 +21,11 @@ func NewRenderer(cfg *Config) *Renderer {
 	return &Renderer{cfg: cfg}
 }
 
-func (r *Renderer) Render(methodMap map[string][]*Method) error {
+func (r *Renderer) renderAllGraph(methodMap map[string][]*Method) (string, error) {
 	g := graphviz.New()
 	graph, err := g.Graph()
 	if err != nil {
-		return xerrors.Errorf("failed to create graphviz graph: %w", err)
+		return "", xerrors.Errorf("failed to create graphviz graph: %w", err)
 	}
 	defer func() {
 		if err := graph.Close(); err != nil {
@@ -36,13 +39,13 @@ func (r *Renderer) Render(methodMap map[string][]*Method) error {
 		subgraph := graph.SubGraph(fmt.Sprintf("cluster%s", service.Name), 1)
 		mtds, err := service.Methods()
 		if err != nil {
-			return xerrors.Errorf("failed to parse proto file: %w", err)
+			return "", xerrors.Errorf("failed to parse proto file: %w", err)
 		}
 		for _, mtd := range mtds {
 			fromName := fmt.Sprintf("%s.%s", service.Name, mtd.Name)
 			from, err := subgraph.CreateNode(fmt.Sprintf("%s.%s.%s", service.Name, service.Name, mtd.Name))
 			if err != nil {
-				return xerrors.Errorf("failed to create node: %w", err)
+				return "", xerrors.Errorf("failed to create node: %w", err)
 			}
 			from.SetLabel(fromName)
 			from.SetShape(cgraph.BoxShape)
@@ -52,13 +55,95 @@ func (r *Renderer) Render(methodMap map[string][]*Method) error {
 			}
 			edgeMap := map[string]struct{}{}
 			if err := r.render(subgraph, service.Name, edgeMap, from, mtd, calledMethods, methodMap); err != nil {
-				return xerrors.Errorf("failed to render graph: %w", err)
+				return "", xerrors.Errorf("failed to render graph: %w", err)
 			}
 		}
 	}
 	var b bytes.Buffer
-	g.Render(graph, graphviz.XDOT, &b)
-	g.RenderFilename(graph, graphviz.SVG, fmt.Sprintf("%s.svg", r.cfg.Output))
+	g.Render(graph, graphviz.SVG, &b)
+	return b.String(), nil
+}
+
+func (r *Renderer) renderServiceGraph(service *Service, methodMap map[string][]*Method) (string, error) {
+	g := graphviz.New()
+	graph, err := g.Graph()
+	if err != nil {
+		return "", xerrors.Errorf("failed to create graphviz graph: %w", err)
+	}
+	defer func() {
+		if err := graph.Close(); err != nil {
+			log.Fatalf("failed to close graphviz graph %s", err)
+		}
+		g.Close()
+	}()
+	graph.SetRankDir(cgraph.LRRank)
+	graph.SetNewRank(true)
+	mtds, err := service.Methods()
+	if err != nil {
+		return "", xerrors.Errorf("failed to parse proto file: %w", err)
+	}
+	for _, mtd := range mtds {
+		fromName := fmt.Sprintf("%s.%s", service.Name, mtd.Name)
+		from, err := graph.CreateNode(fmt.Sprintf("%s.%s.%s", service.Name, service.Name, mtd.Name))
+		if err != nil {
+			return "", xerrors.Errorf("failed to create node: %w", err)
+		}
+		from.SetLabel(fromName)
+		from.SetShape(cgraph.BoxShape)
+		calledMethods, exists := methodMap[mtd.MangledName()]
+		if !exists {
+			continue
+		}
+		edgeMap := map[string]struct{}{}
+		if err := r.render(graph, service.Name, edgeMap, from, mtd, calledMethods, methodMap); err != nil {
+			return "", xerrors.Errorf("failed to render graph: %w", err)
+		}
+	}
+	var b bytes.Buffer
+	g.Render(graph, graphviz.SVG, &b)
+	return b.String(), nil
+}
+
+type serviceGraph struct {
+	Name  string
+	Graph string
+}
+
+type renderParam struct {
+	All      string
+	Services []*serviceGraph
+}
+
+func (r *Renderer) Render(methodMap map[string][]*Method) error {
+	tmpl, err := template.New("graph.tmpl").Parse(outputHTML)
+	if err != nil {
+		return xerrors.Errorf("failed to parse template HTML: %w", err)
+	}
+	all, err := r.renderAllGraph(methodMap)
+	if err != nil {
+		return xerrors.Errorf("failed to render all graph: %w", err)
+	}
+	graphs := []*serviceGraph{}
+	for _, service := range r.cfg.Services {
+		graph, err := r.renderServiceGraph(service, methodMap)
+		if err != nil {
+			return xerrors.Errorf("failed to render service graph: %w", err)
+		}
+		graphs = append(graphs, &serviceGraph{
+			Name:  service.Name,
+			Graph: graph,
+		})
+	}
+	var b bytes.Buffer
+	if err := tmpl.Execute(&b, renderParam{
+		All:      all,
+		Services: graphs,
+	}); err != nil {
+		return xerrors.Errorf("failed to execute template: %w", err)
+	}
+	if err := ioutil.WriteFile(fmt.Sprintf("%s.html", r.cfg.Output), b.Bytes(), 0644); err != nil {
+		return xerrors.Errorf("failed to write %s.html: %w", r.cfg.Output, err)
+	}
 	return nil
 }
 
@@ -95,3 +180,48 @@ func (r *Renderer) render(graph *cgraph.Graph, serviceName string, edgeMap map[s
 	}
 	return nil
 }
+
+const outputHTML = `
+<html>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+  <style type="text/css">
+#list {
+    height:100vh;
+    overflow-y:scroll;
+}
+
+#ref {
+    height: 100vh;
+    overflow-y:scroll;
+}
+  </style>
+  <script type="text/javascript">
+    function selectService(serviceName) {
+        let serviceGraph = document.getElementById(serviceName).innerHTML;
+        document.getElementById("ref").innerHTML = serviceGraph;
+        document.getElementById("title").innerHTML = serviceName + " method dependencies";
+    };
+  </script>
+  <body>
+    <div class="row">
+      <div id="list" class="col-3">
+        <ul class="list-group">
+          <div id="all" style="display:none">{{ .All }}</div>
+          <li class="list-group-item list-group-item-action" onClick="selectService('all')">ALL</li>
+          {{- range .Services }}
+          <div id="{{ .Name }}" style="display:none">{{ .Graph }}</div>
+          <li class="list-group-item list-group-item-action" onClick="selectService('{{ .Name }}')">{{ .Name }}</li>
+          {{- end }}
+        </ul>
+      </div>
+      <div class="col-9">
+        <div>
+          <h3 id="title"></h3>
+          <div id="ref">NONE</div>
+        </div>
+      </div>
+    </div>
+  </body>
+  <script type="text/javascript">selectService("all");</script>
+</html>
+`
